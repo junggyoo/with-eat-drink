13bfd5211827bf21004f1eb8ac84f16270c70edd
junggyoo <jungkyu.bae@meshkorea.net>
2022-06-15 16:50:17 +0900

    feat: Layout 컴포넌트 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from 'next'
+import type { ReactElement } from 'react'
+import { Layout } from '../components/layout'
+import { Main } from './main'
 import { Welcome } from './welcome'
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <Welcome />
+    // <Welcome />
+    <Main />
   )
 }
 
-export default Home
+Home.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
